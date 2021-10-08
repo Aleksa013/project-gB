f9106be49290305e776befc0346258164268de57
Aleksa013 <71268132+Aleksa013@users.noreply.github.com>
2021-10-08 19:06:17 +0300

    homework 6

--- FilterComponent.js
+++ FilterComponent.js
@@ -1,11 +1,9 @@
-Vue.component('filters', {
-    props: ['quest'],
-    template: `
-    <form action="#" class="search-form" @submit.prevent="$parent.$emit('filter', 'userSearch')">
-    <input type="text" class="search-field" v-model="quest">
-    <button type="submit" class="btn-search">
-        <i class="fas fa-search"></i>
-    </button>
-    </form>
-    `
-});
+Vue.component('filters',{
+    template:`
+    <form action="#" class="search-form" @submit.prevent="$parent.filter">
+                <input type="text" class="search-field" v-model="$parent.userSearch">
+                <button type="submit" class="btn-search">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>`
+})
